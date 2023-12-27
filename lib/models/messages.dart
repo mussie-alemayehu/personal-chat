0447b9c0a6a0b928d2c6631e62ebfa90382156b7
Mussie Alemayehu <mussiea2020@gmail.com>
2023-12-27 14:52:27 +0300

    modify message model

--- lib/models/messages.dart
+++ lib/models/messages.dart
@@ -1,66 +1,16 @@
-class Messages {
-  final String message;
+class Message {
   final String sentBy;
-  // final DateTime time;
+  final DateTime time;
+  final String? text;
+  final String? image;
 
-  const Messages({
-    required this.message,
+//  final String? videoMessage;
+
+  const Message({
     required this.sentBy,
-    // this.time = DateTime.now(),
+    required this.time,
+    this.text,
+    this.image,
+    // this.videoMessage,
   });
 }
-
-// class MessagesCollection {
-//   static const List<Messages> list = [
-//     Messages(
-//       message: 'Hi',
-//       isMe: true,
-//     ),
-//     Messages(
-//       message: 'Hello there.',
-//       isMe: false,
-//     ),
-//     Messages(
-//       message: 'Hi, you remember me?',
-//       isMe: true,
-//     ),
-//     Messages(
-//       message: 'Of course I do my boy, how can I forget you?',
-//       isMe: false,
-//     ),
-//     Messages(
-//       message: 'Who am I then',
-//       isMe: true,
-//     ),
-//     Messages(
-//       message:
-//           'Well, you are someone who is rude enough to ask such a question.',
-//       isMe: false,
-//     ),
-//     Messages(
-//       message: 'So you don\'t remember me... I knew it.',
-//       isMe: true,
-//     ),
-//     Messages(
-//       message: 'Hey come on now, tell me who you are so that I know.',
-//       isMe: false,
-//     ),
-//     Messages(
-//       message:
-//           'Nah, you were supposed to know me before I told you, you failed the test.',
-//       isMe: true,
-//     ),
-//     Messages(
-//       message: 'Can I at least know what the test is.',
-//       isMe: false,
-//     ),
-//     Messages(
-//       message: 'You will know soon enough. Goodbye for now.',
-//       isMe: true,
-//     ),
-//     Messages(
-//       message: 'Come on who are you?',
-//       isMe: false,
-//     ),
-//   ];
-// }
